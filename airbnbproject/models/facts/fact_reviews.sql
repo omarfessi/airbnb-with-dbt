cf125f463d519269e4853c07d383c33ebbf4760b
Omar <omarfessy@gmail.com>
2023-07-15 02:28:55 +0200

    Use dbt a macro from external packages to generate surrogate key for facts table

--- airbnbproject/models/facts/fact_reviews.sql
+++ airbnbproject/models/facts/fact_reviews.sql
@@ -5,7 +5,8 @@
     )
 }}
 WITH src_reviews as (SELECT * FROM {{ref('src_reviews')}})
-SELECT * FROM src_reviews
+SELECT {{ dbt_utils.generate_surrogate_key(['LISTING_ID', 'REVIEW_DATE', 'REVIEWER_NAME', 'REVIEW_TEXT']) }} as review_id, 
+* FROM src_reviews
 where review_text is not null
 {% if is_incremental() %}
   and review_date >= (select max(review_date) from {{ this }})
