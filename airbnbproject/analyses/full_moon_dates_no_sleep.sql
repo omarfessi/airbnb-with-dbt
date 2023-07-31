aa81054629f3495a70d1d149596e6aeec1a39b74
Omar <omarfessy@gmail.com>
2023-07-31 03:07:49 +0200

    add exposures to the Preset dashboard

--- airbnbproject/analyses/full_moon_dates_no_sleep.sql
+++ airbnbproject/analyses/full_moon_dates_no_sleep.sql
@@ -1,5 +1,5 @@
 WITH  full_moon_dates_reviews AS
-(SELECT is_full_moon, review_sentiment FROM {{ref("FULL_MOON_REVIEWS")}})
+(SELECT is_full_moon, review_sentiment FROM {{ref("full_moon_reviews")}})
 
 SELECT is_full_moon, review_sentiment, count(*) as count 
 FROM full_moon_dates_reviews
